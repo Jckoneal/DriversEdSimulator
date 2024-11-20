f16dea8c5aba9fc18ac780e980b89d392263a646
Jack O'Neal <jack@kevinandjen.org>
2024-11-20 09:10:10 -0800

    updated camera view

--- Scripts/camera.gd
+++ Scripts/camera.gd
@@ -9,9 +9,9 @@ var start_rotation : Vector3
 var start_position : Vector3
 
 func _ready():
-	start_rotation = rotation
-	start_position = position
+	start_rotation = Vector3(0,0,0)
+	start_position = Vector3(0,1.2,0)
 
 func _process(delta):
-		global_position = follow_this.global_transform.origin 
+		global_position = (follow_this.global_position + start_position)
 		global_rotation = follow_this.global_rotation 
