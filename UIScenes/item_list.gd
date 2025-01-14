e0f3059da3be1e8cec450451fc066086789fae5c
Meredith Duff <meredithduff@Merediths-MacBook-Air.local>
2025-01-14 13:46:38 -0800

    hello

---
+++ UIScenes/item_list.gd
@@ -0,0 +1,11 @@
+extends ItemList
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void: 
+	add_item("blah", null,true)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
